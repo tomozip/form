38c756a3c088a646f358d1085b28d5f26cf798fa
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-03-24 18:02:17 +0900

    registar manager func implement

--- app/controllers/companies_users_controller.rb
+++ app/controllers/companies_users_controller.rb
@@ -3,24 +3,28 @@ class CompaniesUsersController < ApplicationController
     @company = Company.find(params[:company_id])
     @companies_users = CompaniesUser.where(company_id: params[:company_id])
     @users = []
+    @existsManager = false
     @companies_users.each do |companies_user|
+      if companies_user.manager == 'delegate'
+        @existsManager = true
+      end
       user = User.find(companies_user.user_id)
       @users.push({info: user, manager: companies_user.manager, companies_user_id: companies_user.id})
     end
   end
 
   def changeManager
-    # 現代表者update
     present_manager = CompaniesUser.find_by(company_id: params[:company_id], manager: 'delegate')
     present_manager.update_attribute(:manager, 'general')
-    # 代表者add
     new_manager = CompaniesUser.find(params[:id])
     new_manager.update_attribute(:manager, 'delegate')
     redirect_to company_companies_users_path(params[:company_id])
   end
 
-  private
-    def companies_user_params
-      params.require(:companies_user).permit(:user_id, :company_id, :manager)
-    end
+  def registarManager
+    new_manager = CompaniesUser.find(params[:id])
+    new_manager.update_attribute(:manager, 'delegate')
+    redirect_to company_companies_users_path(params[:company_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :companies, only: [:create] do
     resources :companies_users, only: [:index] do
       get 'changeManager', on: :member
+      get 'registarManager', on: :member
     end
   end
 
