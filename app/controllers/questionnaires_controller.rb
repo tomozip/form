b8c3d9b5981f9536abaa8489b5e6671c2aa0e89b
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-03-26 17:58:33 +0900

    questionnaire index page done

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,2 +1,19 @@
 class QuestionnairesController < ApplicationController
+
+  def index
+    @questionnaires = Questionnaire.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'messages/create'
-
-  get 'users/show'
-
-  get 'companies_users/index'
-
-  get 'conpanies_users/index'
-
-  get 'companies/create'
-
-  get 'admins/show'
-
   devise_for :admins, module: :admins
   devise_for :users, module: :users
   devise_scope :user do
@@ -19,12 +7,14 @@ Rails.application.routes.draw do
   end
 
   resources :admins, only: [:show]
+
   resources :companies, only: [:create] do
     resources :companies_users, only: [:index] do
       get 'changeManager', on: :member
       get 'registarManager', on: :member
     end
   end
+
   resources :users, only: [:show, :destroy] do
     get 'mypage', on: :member
     get 'manager', on: :member
@@ -35,6 +25,9 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
+  scope '/admin' do
+    resources :questionnaires, only: [:new, :create, :destroy, :index]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
