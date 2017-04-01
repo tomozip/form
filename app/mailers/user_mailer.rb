02c6b3ea6bcb35a889cb159cdfa55f7d32be4cf4
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-04-01 18:42:58 +0900

    changed mailer url written down to root_path

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url = 'localhost:3000'
+    @url = root_url
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
