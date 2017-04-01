f7fae35ead9205652a5fd087dc0992a2623405fc
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-04-01 13:13:50 +0900

    question_choice Model generated

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :questionnaire
+  has_many :question_choices
   enum category: { 'input' => 0, 'textarea' => 1, 'checkbox' => 2, 'selectbox' => 3, 'radio' => 4 }
 end
