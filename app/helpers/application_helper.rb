c616912a3309ae007225b5ce092f940ddef503b6
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-04-01 20:19:04 +0900

    make user able to see question's category from question_list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def to_text(category)
+    case category
+    when 'input'
+      "自由記入(短文)"
+    when 'textarea'
+      '自由記入'
+    when 'checkbox'
+      '複数選択'
+    when 'selectbox'
+      '一択(セレクトボックス)'
+    when 'radio'
+      '一択(チェックボックス)'
+    end
+  end
+
 end
