d61333be9dc962622b5468be2b5871d394a011d2
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-04-02 23:20:39 +0900

    answer#new&create implemented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,3 +37,53 @@ function questionnaire() {
         }
     });
 }
+
+function answer_new() {
+    $('#normal').on('click', () => {
+        $('.warning').remove();
+        const lists = $('li');
+        const warning = '<div class="warning">回答してください。</div>';
+        var all_values_isFull = true;
+        lists.each((index, element) => {
+          if (value_isNull($(element))) {
+              $(element).after(warning)
+              all_values_isFull = false;
+          }
+        });
+        if (all_values_isFull)
+          $('#new_answer').submit();
+        else return false;
+    });
+    $('#temporary').on('click', () => {
+        $('.warning').remove();
+        const lists = $('li');
+        const warning = '<div class="warning">一つ以上回答してください。</div>';
+        let all_values_isNull = true;
+        lists.each((index, element) => {
+          if (!value_isNull($(element))) all_values_isNull = false;
+        });
+        if (all_values_isNull) {
+          $('ol').prepend(warning);
+          return false;
+        } else {
+          $('#new_answer').submit();
+        }
+    });
+
+    function value_isNull(element) {
+        const category = $(element).children('*:nth-child(2)').prop('type');
+        switch (category) {
+          case 'text':
+          case 'textarea':
+          case 'select-one':
+            return !$(element).children('*:nth-child(2)').val();
+          case 'radio':
+          case 'checkbox':
+              var value_isNull = true;
+              element.children('input[type="' + category + '"]').each((i, e) => {
+                  if ($(e).prop('checked')) value_isNull = false;
+              });
+              return value_isNull;
+        }
+    }
+}
--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -16,12 +16,14 @@ class AnswersController < ApplicationController
   end
 
   def create
-    Answer.create(
-      user_id: params[:user_id],
-      questionnaire_id: params[:questionnaire_id],
-      status: 'answered'
-    )
-    create_question
+    if params[:temporary]
+      # answer_create('answering')
+      create_temporary_question
+    else
+      # answer_create('answered')
+      create_question
+    end
+    redirect_to questionnaire_list_user_questionnaires_path(params[:user_id])
   end
 
   def show
@@ -36,24 +38,117 @@ class AnswersController < ApplicationController
   end
 
   private
+    def answer_create(status)
+      Answer.create(
+        user_id: params[:user_id],
+        questionnaire_id: params[:questionnaire_id],
+        status: status
+      )
+    end
+
     def create_question
       params.require(:answer).each do |key, value|
-        category = params.require(:answer).require(key.to_sym).require(:category)
+        question_answer_id = create_question_answer(key.to_i)
+        category =  params.require(:answer).require(key.to_sym).require(:category)
         case category
-        when 'input'
-        when 'textarea'
+        when 'input', 'textarea'
           answer_text_params = params.require(:answer).require(key.to_sym).permit(:body)
+          answer_text_params[:question_answer_id] = question_answer_id
           AnswerText.create(answer_text_params)
-        when 'selectbox'
-        when 'radio'
+        when 'selectbox', 'radio'
           answer_choice_params = params.require(:answer).require(key.to_sym).permit(:question_choice_id)
+          answer_choice_params[:question_answer_id] = question_answer_id
           AnswerChoice.create(answer_choice_params)
         when 'checkbox'
           params.require(:answer).require(key.to_sym).each do |key2, value2|
+            next if key2 == 'category'
             answer_choice_params = params.require(:answer).require(key.to_sym).require(key2.to_sym).permit(:question_choice_id)
+            answer_choice_params[:question_answer_id] = question_answer_id
             AnswerChoice.create(answer_choice_params)
           end
         end
       end
+    end
+
+    def create_temporary_question
+      params.require(:answer).each do |key, value|
+        category = params.require(:answer).require(key.to_sym).require(:category)
+        case category
+        when 'input', 'textarea'
+          value_isNull = params.require(:answer).require(key.to_sym)[:body].empty?
+          unless value_isNull
+            question_answer_id = create_question_answer(key.to_i)
+            answer_text_params = params.require(:answer).require(key.to_sym).permit(:body)
+            answer_text_params[:question_answer_id] = question_answer_id
+            AnswerText.create(answer_text_params)
+          end
+        when 'selectbox', 'radio'
+          value_isNull = params.require(:answer).require(key.to_sym)[:question_choice_id].blank?
+          unless value_isNull
+            question_answer_id = create_question_answer(key.to_i)
+            answer_choice_params = params.require(:answer).require(key.to_sym).permit(:question_choice_id)
+            answer_choice_params[:question_answer_id] = question_answer_id
+            AnswerChoice.create(answer_choice_params)
+          end
+        when 'checkbox'
+          value_isNull = params.require(:answer).require(key.to_sym).length <= 1
+          unless value_isNull
+            question_answer_id = create_question_answer(key.to_i)
+            params.require(:answer).require(key.to_sym).each do |key2, value2|
+              next if key2 == 'category'
+              answer_choice_params = params.require(:answer).require(key.to_sym).require(key2.to_sym).permit(:question_choice_id)
+              answer_choice_params[:question_answer_id] = question_answer_id
+              AnswerChoice.create(answer_choice_params)
+            end
+          end
+        end
+      end
+    end
+
+    def update_temporary_question
+      params.require(:answer).each do |key, value|
+        category = params.require(:answer).require(key.to_sym).require(:category)
+        case category
+        when 'input', 'textarea'
+          value_isNull = params.require(:answer).require(key.to_sym)[:body].empty?
+          if value_isNull
+            question_answer = QuestionAnswer.find_by(user_id: params[:user_id], question_id: key.to_i)
+            question_answer.destroy
+          else
+            question_answer_id = create_question_answer(key.to_i)
+            answer_text_params = params.require(:answer).require(key.to_sym).permit(:body)
+            answer_text_params[:question_answer_id] = question_answer_id
+            AnswerText.create(answer_text_params)
+          end
+        when 'selectbox', 'radio'
+          value_isNull = params.require(:answer).require(key.to_sym)[:question_choice_id].blank?
+          if value_isNull
+            question_answer = QuestionAnswer.find_by(user_id: params[:user_id], question_id: key.to_i)
+            question_answer.destroy
+          else
+            question_answer_id = create_question_answer(key.to_i)
+            answer_choice_params = params.require(:answer).require(key.to_sym).permit(:question_choice_id)
+            answer_choice_params[:question_answer_id] = question_answer_id
+            AnswerChoice.create(answer_choice_params)
+          end
+        when 'checkbox'
+          value_isNull = params.require(:answer).require(key.to_sym)[:question_choice_id].blank?
+          if value_isNull
+          else
+            params.require(:answer).require(key.to_sym).each do |key2, value2|
+              next if key2 == 'category'
+              question_answer_id = create_question_answer(key.to_i)
+              answer_choice_params = params.require(:answer).require(key.to_sym).require(key2.to_sym).permit(:question_choice_id)
+              answer_choice_params[:question_answer_id] = question_answer_id
+              AnswerChoice.create(answer_choice_params)
+            end
+          end
+        end
+      end
+    end
+
+    def create_question_answer(question_id)
+      question_answer = QuestionAnswer.create(question_id: question_id, user_id: current_user.id)
+      question_answer.id
     end
 end
