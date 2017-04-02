e47ccaa15f9e2ac5def14becffbe142e138140c7
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-04-02 15:32:23 +0900

    implemented answer#index with questionnaire_list

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,33 @@
 class AnswersController < ApplicationController
   def index
+    answers = Answer.where(user_id: params[:user_id])
+    if answers.empty?
+      @questionnaires = nil
+      render
+    else
+      answering_questionnaire_ids = []
+      answered_questionnaire_ids = []
+      answers.each do |answer|
+        questionnaire_id = answer.questionnaire_id
+        if answer.status == 'answering'
+          answering_questionnaire_ids.push(questionnaire_id)
+        else
+          answered_questionnaire_ids.push(questionnaire_id)
+        end
+      end
+      questionnaires = Questionnaire.all
+      @questionnaires = { answered: [], answering: [], not_yet: [] }
+      questionnaires.each do |questionnaire|
+        questionnaire_id = questionnaire.id
+        if answered_questionnaire_ids.include?(questionnaire_id)
+          @questionnaires[:answered].push(questionnaire)
+        elsif answering_questionnaire_ids.include?(questionnaire_id)
+          @questionnaires[:answering].push(questionnaire)
+        else
+          @questionnaires[:not_yet].push(questionnaire)
+        end
+      end
+    end
   end
 
   def show
