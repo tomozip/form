5ec1f7859c5edefc4cd20bf1a65738c06bf0ff13
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-04-09 18:21:35 +0900

    modify about config

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class AdminsController < ApplicationController
   def show
     @companies = Company.all
     @companies_users = []
     @companies.each do |company|
-      # TODO: manager: 0のとこをちゃんと1に！
-      manager = CompaniesUser.where(["company_id = ? and manager = ?", company.id, 1]).select("user_id")[0]
-      if manager
+      manager = CompaniesUser.find_by(company_id: company.id, manager: 'delegate')
+      if manager.present?
         manager_name = User.find(manager.user_id).name
         @companies_users.push(manager_name)
       else
